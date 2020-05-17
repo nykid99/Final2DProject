5a53c231d617e1d8e4b768135a8f592d2819ccf5
nykid99 <56805087+nykid99@users.noreply.github.com>
2020-05-16 20:00:16 -0700

    added coinChest

---
+++ Assets/Scripts/ChestController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    PlayerMovement p1;
+    public AudioClip ChestClip;
+    private bool playerCollision;
+    public bool alreadyPlayed = false;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Sprite openSprite, closedSprite;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            playerCollision = true;
+        }
+        if (playerCollision == true && alreadyPlayed == false)
+        {
+            spriteRenderer.sprite = openSprite;
+            AudioSource.PlayClipAtPoint(ChestClip, transform.position);
+            playerCollision = false;
+            alreadyPlayed = true;
+
+        }
+    }
+}
